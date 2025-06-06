19b9a28c8f888becdd3935911d598bf24f645596
Sourav Menon <45852906+souravmenon1999@users.noreply.github.com>
2025-06-06 07:48:49 +0530

    added

--- framed/exchange/exchange.go
+++
@@ -1,9 +0,0 @@
-package exchange
-
-import "github.com/souravmenon1999/trade-engine/framed/types"
-
-type Exchange interface {
-    SendOrder(order *types.Order) (string, error)
-    CancelOrder(orderID string) error
-    RegisterOrderUpdateCallback(callback func(*types.OrderUpdate))
-}
